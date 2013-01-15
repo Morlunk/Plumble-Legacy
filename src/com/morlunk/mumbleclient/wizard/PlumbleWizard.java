4066b876a2bac3b5451e7667ef38385951f9701b
Andrew Comminos <andrewcomminos@gmail.com>
2013-01-14 18:51:50 -0800

    More work on wizard transitions.

--- src/com/morlunk/mumbleclient/wizard/PlumbleWizard.java
+++ src/com/morlunk/mumbleclient/wizard/PlumbleWizard.java
@@ -1,57 +1,70 @@
 package com.morlunk.mumbleclient.wizard;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import com.morlunk.mumbleclient.R;
 
 public class PlumbleWizard extends FragmentActivity {
 	
+	private ViewFlipper mViewFlipper;
+	private Button mBackButton;
+	private Button mNextButton;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.wizard_intro);
+		setContentView(R.layout.wizard);
 		
-		Button cancelButton = (Button) findViewById(R.id.wizardCancel);
-		cancelButton.setOnClickListener(new OnClickListener() {
+		mViewFlipper = (ViewFlipper) findViewById(R.id.wizardFlipper);
+		mViewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left));
+		mViewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right));
+		
+		mBackButton = (Button) findViewById(R.id.wizardBack);
+		mBackButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				finish();	
+				if(mViewFlipper.getDisplayedChild() == 0) {
+					finish();
+				} else {
+					mViewFlipper.setDisplayedChild(mViewFlipper.getDisplayedChild()-1);
+					updateButtonLabels();
+				}
 			}
 		});
 		
-		Button nextButton = (Button) findViewById(R.id.wizardContinue);
-		nextButton.setOnClickListener(new OnClickListener() {
+		mNextButton = (Button) findViewById(R.id.wizardNext);
+		mNextButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(PlumbleWizard.this, WizardCertificateActivity.class));
+				if(mViewFlipper.getDisplayedChild() == mViewFlipper.getChildCount()-1) {
+					finish();
+				} else {
+					mViewFlipper.setDisplayedChild(mViewFlipper.getDisplayedChild()+1);
+					updateButtonLabels();
+				}
 			}
 		});
+		
+		updateButtonLabels();
 	}
 	
-	public static class WizardCertificateActivity extends FragmentActivity {
-		
-		@Override
-		public void onCreate(Bundle arg0) {
-			super.onCreate(arg0);
-			setContentView(R.layout.wizard_certificate);
+	/**
+	 * Updates the button labels depending on the user's position in the view stack.
+	 */
+	private void updateButtonLabels() {
+		int position = mViewFlipper.getDisplayedChild();
+		int maxPosition = mViewFlipper.getChildCount()-1;
 
-			Button backButton = (Button) findViewById(R.id.wizardBack);
-			backButton.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					finish();
-				}
-			});
-		}
+		mBackButton.setText(position == 0 ? R.string.skip : R.string.back);
+		mNextButton.setText(position == maxPosition ? R.string.finish : R.string.next);
 	}
 }
